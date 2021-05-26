c85494d63edc672d9751892dd28426bd8c750cb3
eddypastika <pastikaeddy@gmail.com>
2021-05-26 23:12:46 +0700

    expenses list separation

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, {useState} from "react"; // we don't need import this in the modern React
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
 
 import './Expenses.css';
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
     const [selectedYear, setSelectedYear] = useState("2021");
@@ -15,25 +15,10 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === selectedYear;
     })
 
-    let expensesContent = <p>No Expense Found.</p>;
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.length > 0 && filteredExpenses.map((item) => (
-            <ExpenseItem
-                key={item.id}
-                title={item.title}
-                amount={item.amount}
-                date={item.date}
-            />
-        ))
-    }
-
     return (
         <Card className="expenses">
             <ExpensesFilter selected={selectedYear} onGetSelectedYearFilter={getSelectedYearFilterHandler}/>
-            {/*{props.items.map(item =>*/}
-            {/*    <ExpenseItem key={item.id} title={item.title} amount={item.amount} date={item.date}/>*/}
-            {/*)}*/}
-            {expensesContent}
+            <ExpensesList items={filteredExpenses}/>
         </Card>
     )
 };
