671d52bf336c060af2c4f4e16428421d482fa853
eddypastika <pastikaeddy@gmail.com>
2021-05-26 09:03:30 +0700

    add expenses component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
     const expenses = [
@@ -8,10 +8,8 @@ function App() {
     ];
     return (
         <div>
-            <h2>Hello!</h2>
-            <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-            <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-            <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
+            <h2>My Expense Tracker</h2>
+            <Expenses items={expenses}/>
         </div>
     );
 }
